7bc15214bad4dfe847277f11cc210ec91b00ac85
i.kłusek <J6ZkqwVUn>
2023-11-12 10:09:25 +0100

    MSG-02-Legend_UI_and_general_styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import 'modern-normalize/modern-normalize.css';
+	import '../styles/main.scss';
+	import favicon from '$assets/images/favicon-32x32.png';
+	import Legend from '$components/Legend.svelte';
+</script>
+
+<svelte:head>
+	<title>Multi-Step-Form</title>
+	<link rel="icon" type="image/svg" href={favicon} />
+</svelte:head>
+
+<div class="general-wrapper">
+	<Legend />
+	<slot />
+</div>
+
+<style lang="scss">
+	.general-wrapper {
+		border-radius: var(--border-radius-general);
+		padding: 15px;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		gap: 10px;
+		background-color: var(--color-neutral-white);
+	}
+</style>
